b00d45d051fae334ed0e301506cb7d295718ff04
Hamza.Noah <hamza.nouh97@gmail.com>
2023-12-12 19:48:57 +0200

    linked the current repo to the remote repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sapiente iure, asperiores atque ipsum in expedita? Nisi non illum repudiandae suscipit ex ullam iusto eaque laborum in! Consequatur, delectus officiis.</>;
+}
+
+export default App;

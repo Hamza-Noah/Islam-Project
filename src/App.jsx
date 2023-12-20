82c3c8758f50847a0d94dc02ac4596aac53d328e
Hamza.Noah <hamza.nouh97@gmail.com>
2023-12-20 22:10:54 +0200

    added the skillset

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
+import Skillset from "./components/Skillset";
+
 import { changeThemeToDark, darkMode } from "./components/RTK/slice";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -16,16 +18,20 @@ function App() {
       <section
         style={{
           background: mode ? "white" : "",
-          color: mode ? "black" : "",
+          // color: mode ? "black" : "",
         }}
         className="all-template"
       >
         <Header />
         <Landing />
+        <Skillset />
       </section>
-        <button className="change-theme" onClick={() => dispatch(changeThemeToDark())}>
-          <h1>dark mode</h1>
-        </button>
+      <button
+        className="change-theme"
+        onClick={() => dispatch(changeThemeToDark())}
+      >
+        <h1>dark mode</h1>
+      </button>
     </>
   );
 }
---
+++ src/components/Skillset/Skillset.jsx
@@ -0,0 +1,45 @@
+import styles from "./skilleset.module.css";
+
+export default function Skillset() {
+  return (
+    <>
+      <section className="mt-5 pt-5"> 
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <hgroup>
+                <p
+                  className={`${styles["special-color"]} fw-bolder text-uppercase`}
+                >
+                  my skillset
+                </p>
+                <h2 className={`${styles["title-color"]} fw-bolder`}>
+                  <span className="d-block">Graphic Design,</span>
+                  <span className="d-block">Interface Design &</span>
+                  <span className="d-block">User Experince</span>
+                </h2>
+              </hgroup>
+            </div>
+            <div className="col-lg-6">
+              <div className={`${styles.info}`}>
+                <p className="text-white">
+                  I specialize in building complex web applications, leading
+                  front-end teams, digital product design and developing visual
+                  design systems. I enjoy creating effortless user experience
+                  and designing delightful digital products. The entire process
+                  of going from a concept to release and gathering user’s
+                  feedback on either client’s or my own products is what makes
+                  me wake up everyday. I worked with numerous clients from all
+                  around the world from early startups to well- established
+                  companies. I always seek new opportunities for cooperation on
+                  projects around interesting dashboards, design systems or
+                  landing pages. Let’s create something awesome together!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}

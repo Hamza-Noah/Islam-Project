0561c3a2aaf26b06812e081c6b389e92e8386f97
Hamza.Noah <hamza.nouh97@gmail.com>
2023-12-23 16:48:28 +0200

    added the icons to the

--- src/components/WhatIsNext/WhatIsNext.jsx
+++ src/components/WhatIsNext/WhatIsNext.jsx
@@ -1,6 +1,15 @@
 import styles from "./what-is-next.module.css";
+import socialIcon1 from "../../assets/images/svg/social-icon-1.svg";
+import socialIcon2 from "../../assets/images/svg/social-icon-2.svg";
+import socialIcon3 from "../../assets/images/svg/social-icon-3.svg";
+import socialIcon4 from "../../assets/images/svg/social-icon-4.svg";
+import socialIcon5 from "../../assets/images/svg/social-icon-5.svg";
+import socialIcon6 from "../../assets/images/svg/social-icon-6.svg";
+
 
 export default function WhatNext() {
+  let socialIcons = [socialIcon1, socialIcon2, socialIcon3, socialIcon4, socialIcon5, socialIcon6];
+
   return (
     <section className={`${styles["what-next"]} text-center`}>
       <div className="container">
@@ -18,6 +27,13 @@ export default function WhatNext() {
           If you'd like to talk about a project you want help with or need an
           advice about product design, just drop me{" "}
         </p>
+        <div className={`${styles["social-links"]}`}>
+          <ul className="d-flex justify-content-center list-unstyled">
+           {socialIcons.map((socialIcon, i) => {
+           return <li className={styles.icon} key={i}><a href="#"><img src={socialIcon}/></a></li>
+           })}
+          </ul>
+        </div>
       </div>
     </section>
   );

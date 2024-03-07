4b77de39dcbe10cce68436122c08e3055c121415
Hamza-Noah <hamza.nouh97@gmail.com>
2024-03-07 11:02:38 +0200

    enabled changing theme in the landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,36 +1,72 @@
+import { useState, useEffect } from "react";
 import styles from "./landing.module.css";
 
-export default function Landing() {
+export default function Landing(props) {
+  const [theme, setTheme] = useState();
+
+  function applyThemeClass(theme, styles) {
+    return theme === "purple"
+      ? styles.purple
+      : theme === "dark"
+      ? styles.dark
+      : styles.white;
+  }
+
+  useEffect(() => {
+    // const logos = {
+    //   dark: logoBlack,
+    //   white: logoWhite,
+    //   default: logoPruple,
+    // };
+    // setLogo(logos[props.mode]);
+    setTheme(props.mode);
+  }, [props.mode]);
+
   return (
-    <div className="container">
-      <section className={styles.landing}>
-        <div className="row">
-          <div className="col-lg-6 pt-5">
-            <div className={`${styles.info} position-relative pt-5 mt-5 text-white`}>
-              <p
-                className={`${styles["job-title"]}  position-absolute d-none d-lg-flex  flex-column`}
+    <div className="position-relative">
+      <div className="container">
+        <section
+          className={`${styles.landing} ${applyThemeClass(theme, styles)}`}
+        >
+          <div className="row">
+            <div className="col-lg-6 pt-5">
+              <div
+                className={`${styles.info} position-relative pt-5 mt-5 text-white`}
               >
-                <span>Product</span>
-                <span className="ps-lg-5">Desiner</span>
-              </p>
-              <div className="position-relative">
-                <p className={`${styles.bow} fw-bolder`}>Hello it's me</p>
-                <h1 className={`${(styles.title, styles["designer-name"])} `}>
-                  Eslam Abbas
-                </h1>
-                <p className={styles.details}>
-                  An enthusiastic product designer currently shaping the future
-                  of software development by designing smooth user-interfaces
-                  that promote user interaction with information and data. While
-                  traveling around the world.
+                <p
+                  className={`${styles["job-title"]} ${applyThemeClass(theme, styles)}  position-absolute d-none d-lg-flex  flex-column`}
+                >
+                  <span>Product</span>
+                  <span className="ps-lg-5">Desiner</span>
                 </p>
+                <div className="position-relative">
+                  <p className={`${styles.bow} fw-bolder`}>Hello it's me</p>
+                  <h1 className={`${(styles.title, styles["designer-name"])} ${applyThemeClass(theme, styles)}`}>
+                    Eslam Abbas
+                  </h1>
+                  <p className={`${styles.details} ${applyThemeClass(theme, styles)}`}>
+                    An enthusiastic product designer currently shaping the
+                    future of software development by designing smooth
+                    user-interfaces that promote user interaction with
+                    information and data. While traveling around the world.
+                  </p>
+                </div>
+                <a
+                  href="#skillset"
+                  className={`text-uppercase ${styles["link-to"]} ${applyThemeClass(theme, styles)}`}
+                >
+                  scroll for more
+                </a>
               </div>
-              <a href="#skillset" className={`text-uppercase ${styles["link-to"]}`}>scroll for more</a>
+            </div>
+            <div className="col">
+              {theme === "dark" && (
+                <div className={`h-100 ${styles["bg-landing"]}`}></div>
+              )}
             </div>
           </div>
-          <div className="col"></div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 }

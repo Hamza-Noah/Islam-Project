c12e15ce85cf1331d4283fd77e789a19f4148ef9
Hamza-Noah <hamza.nouh97@gmail.com>
2024-03-19 05:49:42 +0200

    finished the circadian

--- src/components/ThemeChanger/ThemeChanger.jsx
+++ src/components/ThemeChanger/ThemeChanger.jsx
@@ -1,92 +1,84 @@
-// Your ThemeChanger component
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./themeChanger.module.css";
 
 import iconPurple from "../../assets/images/svg/purple-theme-icon.svg";
+import iconWhite from "../../assets/images/svg/white-theme-icon.svg";
+import iconDark from "../../assets/images/svg/dark-theme-icon.svg";
+
 import purpleLine from "../../assets/images/svg/purple-line.svg";
 import blueLine from "../../assets/images/svg/blue-line.svg";
 import yellowLine from "../../assets/images/svg/yellow-line.svg";
 
 export default function ThemeChanger(props) {
-  const [activePurpleIndex, setActivePurpleIndex] = useState(-1); // State to store the index of the active purple image
-  const [activeWhiteIndex, setActiveWhiteIndex] = useState(-1); // State to store the index of the active white image
-  const [activeDarkIndex, setActiveDarkIndex] = useState(-1); // State to store the index of the active dark image
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [theme, setTheme] = useState("");
+  const [themeIcon, setThemeIcon] = useState();
 
-  // Function to handle theme change for purple images
-  const handlePurpleThemeChange = (index) => {
-    setActivePurpleIndex(index === activePurpleIndex ? -1 : index); // Toggle active index for purple images
-    setActiveWhiteIndex(-1); // Deactivate white images
-    setActiveDarkIndex(-1); // Deactivate dark images
-    props.onThemeChange("purple"); // Trigger theme change callback
+  const handleThemeChange = (themeName, index) => {
+    setActiveIndex(index);
+    setTheme(themeName);
+    props.onThemeChange(themeName);
+    setThemeIcon(icons[theme]);
   };
 
-  // Function to handle theme change for white images
-  const handleWhiteThemeChange = (index) => {
-    setActiveWhiteIndex(index === activeWhiteIndex ? -1 : index); // Toggle active index for white images
-    setActivePurpleIndex(-1); // Deactivate purple images
-    setActiveDarkIndex(-1); // Deactivate dark images
-    props.onThemeChange("white"); // Trigger theme change callback
-  };
+  console.log(theme);
 
-  // Function to handle theme change for dark images
-  const handleDarkThemeChange = (index) => {
-    setActiveDarkIndex(index === activeDarkIndex ? -1 : index); // Toggle active index for dark images
-    setActivePurpleIndex(-1); // Deactivate purple images
-    setActiveWhiteIndex(-1); // Deactivate white images
-    props.onThemeChange("dark"); // Trigger theme change callback
+  useEffect(() => {
+    const icons = {
+      dark: iconDark,
+      white: iconWhite,
+      purple: iconPurple,
+    };
+    // setIcon();
+    setThemeIcon(icons[theme]);
+  }, [theme]);
+
+  const renderHour = (index, imageSrc, imageAlt, themeName) => {
+    const isActive = index === activeIndex && theme === themeName;
+    const isPrevActive = index === activeIndex - 1 && theme === themeName;
+    const isNextActive = index === activeIndex + 1 && theme === themeName;
+
+    return (
+      <div
+        key={index}
+        className={style.hour}
+        onClick={() => handleThemeChange(themeName, index)}
+      >
+        <img
+          src={imageSrc}
+          alt={imageAlt}
+          className={`${isActive ? style.active : ""} ${
+            isPrevActive || isNextActive ? style.taller : ""
+          }`}
+        />
+        {isActive && (
+          <button
+            className={`d-block mx-auto  ${style.hourButton} ${style["theme-btn-purple"]}`}
+          >
+            <i>
+              <img src={themeIcon} alt="" />
+            </i>
+          </button>
+        )}
+      </div>
+    );
   };
 
   return (
-    <section className={`${style["theme-container"]}`}>
+    <section className={`${style["theme-container"]} ${style[theme]}`}>
       <div className="container">
         <div className="theme-settings">
-          <button className={`d-block mx-auto ${style["theme-btn-purple"]}`}>
-            <i>
-              <img src={iconPurple} alt="" />
-            </i>
-          </button>
           <div className={style.circadian}>
             <div className={style.hours}>
-              {[...Array(8)].map((_, index) => (
-                <div
-                  key={index}
-                  className={style.hour}
-                  onClick={() => handleDarkThemeChange(index)}
-                >
-                  <img
-                    src={blueLine}
-                    alt=""
-                    className={index === activeDarkIndex ? style.active : ""}
-                  />
-                </div>
-              ))}
-              {[...Array(8)].map((_, index) => (
-                <div
-                  key={index}
-                  className={style.hour}
-                  onClick={() => handlePurpleThemeChange(index)}
-                >
-                  <img
-                    src={purpleLine}
-                    alt=""
-                    className={index === activePurpleIndex ? style.active : ""}
-                  />
-                </div>
-              ))}
-              {[...Array(8)].map((_, index) => (
-                <div
-                  key={index}
-                  className={style.hour}
-                  onClick={() => handleWhiteThemeChange(index)}
-                >
-                  <img
-                    src={yellowLine}
-                    alt=""
-                    className={index === activeWhiteIndex ? style.active : ""}
-                  />
-                </div>
-              ))}
+              {[...Array(8)].map((_, index) =>
+                renderHour(index, blueLine, "", "dark")
+              )}
+              {[...Array(8)].map((_, index) =>
+                renderHour(index, purpleLine, "", "purple")
+              )}
+              {[...Array(8)].map((_, index) =>
+                renderHour(index, yellowLine, "", "white")
+              )}
             </div>
           </div>
         </div>

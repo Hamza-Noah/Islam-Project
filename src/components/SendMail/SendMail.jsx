9289f830a135caf4420c0660c41b6298748192ce
Hamza-Noah <hamza.nouh97@gmail.com>
2024-03-28 17:21:01 +0200

    added the glassy background to the modal

--- src/components/SendMail/SendMail.jsx
+++ src/components/SendMail/SendMail.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 import style from "./send-mail.module.css";
-
 
 export default function SendMail() {
   return (
-    
-      <div
-            className={`${style.modal} modal fade`}
-            id="exampleModal"
-            tabIndex={-1}
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div className={`modal-dialog ${style["modal-dialog"]}`}>
-              <div className={`modal-content bg-dark ${style["modal-content"]}`} >
-                <div className="modal-body"></div>
-                    <h2>Contact Form</h2>
-              </div>
-            </div>
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex={-1}
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog ">
+        <div className="modal-content">
+          <div className="modal-body">
+            <h2>Contact Form</h2>
           </div>
-    )
+        </div>
+      </div>
+    </div>
+  );
 }

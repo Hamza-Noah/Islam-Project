7586fa09e478a0a9d9ca5340c71e16f7a9d57c4e
Hamza-Noah <hamza.nouh97@gmail.com>
2024-03-29 21:25:09 +0200

    added formspree/react, linked the form to my personal mail & tested messages correctly

--- src/components/SendMail/SendMail.jsx
+++ src/components/SendMail/SendMail.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import style from "./send-mail.module.css";
 
 export default function SendMail() {
+  const [state, handleSubmit] = useForm("mwkgrqne");
+  if (state.succeeded) {
+    return
+  }
+
   return (
     <div
       className="modal fade"
@@ -13,7 +18,35 @@ export default function SendMail() {
       <div className="modal-dialog ">
         <div className="modal-content">
           <div className="modal-body">
-            <h2>Contact Form</h2>
+            <h2 className="fw-bolder">Contact Form</h2>
+            <form onSubmit={handleSubmit}>
+              {/* <div className="from-group">
+                <input type="text" placeholder="Enter Your Name" />
+              </div> */}
+              <div className="from-group">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
+              </div>
+              <div className="from-group">
+                <textarea name="message" placeholder="Your Message"></textarea>
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                />
+              </div>
+              <button type="submit" disabled={state.submitting}>
+                Submit Your Message
+              </button>
+            </form>
           </div>
         </div>
       </div>
